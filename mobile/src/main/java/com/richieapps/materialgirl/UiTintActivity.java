c2b9bd48bdb66179f1ac7e7f338ff16888c64e2a
seanKenkeremath <sean.kenkeremath@willowtreeapps.com>
2014-11-14 13:27:53 -0500

    added UiTintActivity

---
+++ mobile/src/main/java/com/richieapps/materialgirl/UiTintActivity.java
@@ -0,0 +1,16 @@
+package com.richieapps.materialgirl;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by sean.kenkeremath on 11/14/14.
+ */
+public class UiTintActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_tint);
+    }
+}
